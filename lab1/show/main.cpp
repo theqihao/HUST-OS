99364a1f660794af2c709d8ffa8c6e0cca300ea0
qihaogit <2390631000@qq.com>
2017-02-27 23:27:59 +0800

    add time

--- lab1/show/main.cpp
+++ lab1/show/main.cpp
@@ -1,5 +1,7 @@
 #include <QPushButton>
 #include <QApplication>
+#include <QLabel>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
@@ -8,5 +10,9 @@ int main(int argc, char *argv[])
     QPushButton btn("hello world");
     btn.resize(200,100);
     btn.show();
+
+	QTimer *timer = new QTimer(this);  
+   	connect(timer,SIGNAL(timeout()),this,SLOT(timeoutslot()));//timeoutslot()为自定义槽  
+   	timer->start(1000);  
     return app.exec();
 }
